e93a51df5821c5794f58cf4910bca71cc0eff7f7
Marina Medhat <145957468+marina5518@users.noreply.github.com>
2026-05-09 07:30:00 +0300

    Modify preprocess_image to include normalization
    Added normalization option to preprocess_image function.

--- preprocessing.py
+++ preprocessing.py
@@ -1,16 +1,8 @@
-from PIL import Image
-import numpy as np
-
-IMG_SIZE = 224
-
-def preprocess_image(image):
-
+def preprocess_image(image, normalize=True):
     image = image.convert("RGB")
-
     image = image.resize((IMG_SIZE, IMG_SIZE))
-
     image = np.array(image)
-
+    if normalize:
+        image = image / 255.0   # Only for MobileNetV2
     image = np.expand_dims(image, axis=0)
-
     return image
